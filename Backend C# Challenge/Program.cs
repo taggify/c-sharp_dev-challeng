2a678534f42b1f8ae64a22747e467450de02457e
Critian <sanchez.cn@hotmail.com>
2023-10-24 15:59:44 -0300

    configurar-conexion-sqlite

--- Backend C# Challenge/Program.cs
+++ Backend C# Challenge/Program.cs
@@ -6,8 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-    opt.UseInMemoryDatabase("ProductDatabase"));
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); // Utiliza GetConnectionString
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
