7ed6f093c34d8c2ebfd029ca873ae3b31f0df994
Critian <sanchez.cn@hotmail.com>
2023-10-24 14:32:01 -0300

    crear-modelo-y-dbcontext

---
+++ Backend C# Challenge/Models/Product.cs
@@ -0,0 +1,16 @@
+namespace Backend_C_Challenge.Models;
+
+public class Product
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public float Price { get; set; }
+
+    public int Stock { get; set; }
+
+    
+}
---
+++ Backend C# Challenge/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_C_Challenge.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = null!;
+}
+
+
